97f80a29cd1df0346bb8758692a2886917b89213
dingli0770 <45971099+dingli0770@users.noreply.github.com>
2019-01-02 22:56:54 +0800

    Update 净利润.py

--- 净利润.py
+++ 净利润.py
@@ -1,7 +1,7 @@
  i = int(raw_input('¾»ÀûÈó:'))
  arr = [1000000,600000,400000,200000,100000,0]
  rat = [0.01,0.015,0.03,0.05,0.075,0.1]
- r = 0 
+ r = 0  
  for idx in range(0,6):
      if i>arr[idx]:
          r+=(i-arr[idx])*rat[idx]
